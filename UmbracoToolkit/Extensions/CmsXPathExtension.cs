e9599fb52d43e06f8b9d1600db0cb3c5f91ace24
Quang Phan <oclockvn@gmail.com>
2018-06-12 14:56:49 +0700

    updating xpath extension

---
+++ UmbracoToolkit/Extensions/CmsXPathExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UmbracoToolkit.Helpers;
+
+namespace UmbracoToolkit.Extensions
+{
+    public static class CmsXPathExtension
+    {
+        public static CmsXPath WithRoot(this CmsXPath xpath)
+        {
+            xpath.XPath = "/";
+            return xpath;
+        }
+
+        public static CmsXPath WithChild(this CmsXPath xpath, string child, string where = null)
+        {
+            xpath.XPath += child;
+
+            return xpath;
+        }
+    }
+}
---
+++ UmbracoToolkit/Helpers/CmsXPath.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UmbracoToolkit.Helpers
+{
+    public class CmsXPath
+    {
+        public string XPath { get; set; }
+
+        public static CmsXPath Begin => new CmsXPath();
+    }
+}
