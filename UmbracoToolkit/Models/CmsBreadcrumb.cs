035cd23d36f0aaab505cab1a071731adbeb0a84c
Quang Phan <oclockvn@gmail.com>
2018-06-12 12:21:55 +0700

    added breadcrumb

---
+++ UmbracoToolkit/Models/CmsBreadcrumb.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace UmbracoToolkit.Models
+{
+    public class CmsBreadcrumb : CmsModelBase
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is homepage.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this instance is homepage; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsHomepage { get; set; }
+
+        /// <summary>
+        /// Gets or sets the parts.
+        /// </summary>
+        /// <value>
+        /// The parts.
+        /// </value>
+        public List<CmsModelBase> Parts { get; set; }
+
+        /// <summary>
+        /// Gets or sets the current.
+        /// </summary>
+        /// <value>
+        /// The current.
+        /// </value>
+        public CmsModelBase Current { get; set; }
+    }
+}
