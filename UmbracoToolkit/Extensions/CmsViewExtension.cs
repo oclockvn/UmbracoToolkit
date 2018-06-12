2bb21a5cbfede747193ade1619e3e2d9f916a814
Quang Phan <oclockvn@gmail.com>
2018-06-12 13:35:52 +0700

    added view extension

---
+++ UmbracoToolkit/Extensions/CmsViewExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UmbracoToolkit.Extensions
+{
+    public static class CmsViewExtension
+    {
+        /// <summary>
+        /// Time ago html helper.
+        /// </summary>
+        /// <param name="helper">The helper.</param>
+        /// <param name="time">The time.</param>
+        /// <param name="format">The format.</param>
+        /// <returns></returns>
+        public static IHtmlString TimeAgo(this HtmlHelper helper, DateTime time, string format = "MM/dd/yyyy hh:mm:ss tt")
+        {
+            return new HtmlString($@"<time datetime='{time.ToString(format, System.Globalization.CultureInfo.InvariantCulture)}' class='timeago'></time>");
+        }
+    }
+}
